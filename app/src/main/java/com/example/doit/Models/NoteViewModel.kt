ac927d695d43c0ef9ec1027b4488845fb6dd1c8f
Darek <daarek.dajcz@gmail.com>
2023-04-08 10:27:11 +0200

    MainActivity changes

---
+++ app/src/main/java/com/example/doit/Models/NoteViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.doit.Models
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.doit.Database.NoteDatabase
+import com.example.doit.Database.NotesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: NotesRepository
+    val allNotes: LiveData<List<Note>>
+
+    init {
+        val dao = NoteDatabase.getDatabase(application).getNoteDao()
+        repository = NotesRepository(dao)
+        allNotes = repository.allNotes
+    }
+
+    fun deleteNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(note)
+    }
+
+    fun insertNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(note)
+    }
+
+    fun updateNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.update(note)
+    }
+
+
+}
